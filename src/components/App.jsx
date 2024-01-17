e0287a0f89baac37e2589591b7ad0b372dd18908
Ania <124797307+aniakurtiak@users.noreply.github.com>
2024-01-17 02:00:04 +0200

    add nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout/Layout"
+import { lazy } from "react";
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
+const Favorites = lazy(() => import('../pages/Favorites/Favorites'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/catalog" element={<Catalog />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Route>
+  </Routes>
   );
 };
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import { GlobalStyle } from 'components/GlobalStyle';
+import { Suspense } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/catalog">Catalog</NavLink>
+          <NavLink to="/favorites">/Favorites</NavLink>
+        </nav>
+      </header>
+      <Suspense>
+        <Outlet />
+      </Suspense>
+      <GlobalStyle />
+    </div>
+  );
+};
