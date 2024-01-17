99d1404838fb13d3e58dd9fda521b1af4313a96a
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2024-01-17 00:00:59 +0000

    Deploying to gh-pages from @ aniakurtiak/car-rental@e0287a0f89baac37e2589591b7ad0b372dd18908 🚀

---
+++ static/js/544.0d0aa127.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=544.0d0aa127.chunk.js.map
