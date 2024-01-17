26a05ef9b0bf974e6da1975ec48d79c8fd062aaa
Ania <124797307+aniakurtiak@users.noreply.github.com>
2024-01-18 01:42:12 +0200

    change name

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter basename="/car-rental">
+        <BrowserRouter basename="/cars-rental">
           <ThemeProvider theme = {theme}>
             <App />
           </ThemeProvider>
