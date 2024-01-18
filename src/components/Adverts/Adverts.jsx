0f9d1915cc28602b10c0ec2c5b8e0ca0fe9c85f7
Ania <124797307+aniakurtiak@users.noreply.github.com>
2024-01-18 17:04:06 +0200

    add Filter Component

---
+++ src/components/Adverts/Adverts.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAdverts } from '../../redux/operations';
+import { selectAdverts } from '../../redux/selectors';
+
+export const Adverts = () => {
+  const dispatch = useDispatch();
+  const adverts = useSelector(selectAdverts);
+
+  useEffect(() => {
+    dispatch(fetchAdverts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ul>
+        {adverts.map(advert => (
+          <li key={advert.id}>
+            <div>
+              <img src={advert.img} alt="car" />
+            </div>
+            <div>
+              {advert.make}
+              {advert.model}
+              {advert.year}
+              {advert.rentalPrice}
+            </div>
+            <div>
+              {advert.address}
+              {advert.rentalCompany}
+              {advert.type}
+              {advert.model}
+              {advert.id}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,48 +1,13 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAdverts } from '../../redux/operations';
-import { selectAdverts } from '../../redux/selectors';
+import { Adverts } from 'components/Adverts/Adverts';
+import { Filter } from 'components/Filter/Filter';
 
 const Catalog = () => {
-  const dispatch = useDispatch();
-  const adverts = useSelector(selectAdverts);
-
-  useEffect(() => {
-    dispatch(fetchAdverts());
-  }, [dispatch]);
-
   return (
     <div>
-      <ul>
-        {adverts.map(advert => (
-          <li key={advert.id}>
-            <div>
-              <img src={advert.img} alt="car" />
-              </div>
-            <div>
-              {advert.make}
-              {advert.model}
-              {advert.year}
-              {advert.rentalPrice}
-            </div>
-            <div>
-              {advert.address}
-              {advert.rentalCompany}
-              {advert.type}
-              {advert.model}
-              {advert.id}
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Filter />
+      <Adverts />
     </div>
   );
 };
 
 export default Catalog;
-
-
-// <img
-//                 src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-//                 alt="Poster"
-//               />
